280e11d6348622c79019b216390aa0cd57a61845
Salo Soja Edwin <80523850+Dawn-Of-Justice@users.noreply.github.com>
2023-04-30 19:38:21 +0530

    Add files via upload

---
+++ execv/execvDemo.c
@@ -0,0 +1,8 @@
+#include<unistd.h>
+#include<stdio.h>
+
+void main(){
+
+	printf("Bonjour,I am execvDemo & I have been called using execv\n"); //Run gcc execvDemo.c -o execvDemo to make a executable file for EXEC.c 
+
+}
